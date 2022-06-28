3e4740bb9d4af6ce508e63b7ef52bee895af9121
Bella Makhova <rgvder@gmail.com>
2022-06-28 21:24:19 +0300

    refactor: TypeScript loader.ts typing

---
+++ migration-newip-to-ts/src/components/base/base.ts
@@ -0,0 +1,48 @@
+export interface Options {
+    [key: string]: string;
+}
+
+export interface Config<T> {
+    endpoint: string;
+    callback?: (data: T) => void;
+    options?: Options;
+}
+
+interface SourcesItem {
+    category: string;
+    country: string;
+    description: string;
+    id: string;
+    language: string;
+    name: string;
+    url: string;
+}
+
+export interface SourcesData {
+    status: string;
+    sources: SourcesItem[];
+}
+
+interface ArticleSource {
+    id: string;
+    name: string;
+}
+
+interface ArticleItem {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    source: ArticleSource;
+    title: string;
+    url: string;
+    urlToImage: string;
+}
+
+export interface ArticleData {
+    status: string;
+    totalResults: number;
+    articles: ArticleItem[];
+}
+
+export type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'HEAD' | 'CONNECT' | 'OPTIONS' | 'TRACE' | 'PATCH';
